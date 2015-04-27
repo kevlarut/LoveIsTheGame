9e9c01cd59e0d4be273f40cf2b03bd33f27c09fb
Kevin L. Owens <kowens@gmail.com>
2015-04-27 08:26:53 -0600

    Show Level 1 title text

---
+++ Love is the Game/Assets/Scripts/UI/Fade.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class Fade : MonoBehaviour
+    {
+        public float FadeSpeed;
+        public SpriteRenderer SpriteRenderer;
+
+        private float _alpha = 2f;
+
+        // Use this for initialization
+        void Start () {
+	
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            _alpha -= FadeSpeed*Time.deltaTime;
+            if (_alpha <= 1f)
+            {
+                var color = SpriteRenderer.material.color;
+                color.a = _alpha;
+                SpriteRenderer.material.color = color;
+            }
+        }
+    }
+}
